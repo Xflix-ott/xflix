cf98b61663257900a064ed2d1d66878c19512cf0
Xflix-ott <110891938+Xflix-ott@users.noreply.github.com>
2022-08-09 13:17:43 +0530

    Create index.php

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>XFLiX OTT</title>
+</head>
+<body>
+        <h1>Welcome to Official xflix ott | Website under construction</h1>
+</body>
+</html>
